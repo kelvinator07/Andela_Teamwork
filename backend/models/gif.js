152070e997c61fda08af76e656ec0e7a01cd0f2a
Kelvin Isievwore <kisievwore@seamfix.com>
2019-11-19 21:09:50 +0100

    feat(createGif): Added Model For Gifs

---
+++ backend/models/gif.js
@@ -0,0 +1,10 @@
+export default class Gif {
+  constructor(id, ownerId, title, comment, authorName, imageUrl) {
+    this.id = id;
+    this.ownerId = ownerId;
+    this.title = title;
+    this.comment = comment;
+    this.authorName = authorName;
+    this.imageUrl = imageUrl;
+  }
+}
