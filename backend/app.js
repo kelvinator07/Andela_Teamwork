3a29a27d090c1ea94608e679285d280ec820f2eb
Kelvin Isievwore <kisievwore@seamfix.com>
2019-11-19 21:12:04 +0100

    feat(createGif): Added Gif Route To App.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import gifRoutes from './routes/api/gif';
 import userRoutes from './routes/api/user';
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({
   extended: false,
 }));
 
+app.use('/api/v1', gifRoutes);
 app.use('/api/v1/auth', userRoutes);
+
 
 export default app;
