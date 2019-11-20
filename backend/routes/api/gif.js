bf2ea14e2c61faa418275f5d237f57c93d926de4
Kelvin Isievwore <kisievwore@seamfix.com>
2019-11-20 14:37:55 +0100

    chore(update): Updated Gif.js

--- backend/routes/api/gif.js
+++ backend/routes/api/gif.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
 const gifCtrl = require('../../controllers/gif');
 
-router.post('/gifs', gifCtrl.createGif);
+router.post('/gifs', auth, gifCtrl.createGif);
 
 
 export default router;
