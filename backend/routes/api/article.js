8ec357c6318ef380d4acac5595e7e9afea12c571
Kelvin Isievwore <kisievwore@seamfix.com>
2019-11-20 15:57:46 +0100

    feat(deleteArticle): Implemented Route For Delete Article

--- backend/routes/api/article.js
+++ backend/routes/api/article.js
@@ -8,6 +8,7 @@ const articleCtrl = require('../../controllers/article');
 
 router.post('/articles', auth, articleCtrl.createArticle);
 router.put('/articles/:id', auth, articleCtrl.editArticle);
+router.delete('/articles/:id', auth, articleCtrl.deleteArticle);
 
 
 export default router;
