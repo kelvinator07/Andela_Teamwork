cd86327b0a8c3d45af38692bfb8b4e2c954342f3
Kelvin Isievwore <kisievwore@seamfix.com>
2019-11-20 22:20:07 +0100

    feat(allFeeds): Implemented Feed Route To app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import feedRoutes from './routes/api/feed';
 import articleRoutes from './routes/api/article';
 import gifRoutes from './routes/api/gif';
 import userRoutes from './routes/api/user';
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({
   extended: false,
 }));
 
+app.use('/api/v1', feedRoutes);
 app.use('/api/v1', articleRoutes);
 app.use('/api/v1', gifRoutes);
 app.use('/api/v1/auth', userRoutes);
