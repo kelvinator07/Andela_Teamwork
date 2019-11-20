293096668142c2887f350d411a5c686b5a7a6f35
Kelvin Isievwore <kisievwore@seamfix.com>
2019-11-20 17:56:28 +0100

    feat(commentArticle): Implemented Route For Comment

--- backend/routes/api/article.js
+++ backend/routes/api/article.js
@@ -9,6 +9,7 @@ const articleCtrl = require('../../controllers/article');
 router.post('/articles', auth, articleCtrl.createArticle);
 router.put('/articles/:id', auth, articleCtrl.editArticle);
 router.delete('/articles/:id', auth, articleCtrl.deleteArticle);
+router.post('/articles/:id/comment', auth, articleCtrl.commentArticle);
 
 
 export default router;
