6f5b4780ebd6a33c25509d3db6cc66a89f046c4a
Kelvin Isievwore <kisievwore@seamfix.com>
2019-11-20 22:10:48 +0100

    feat(updateArticle): Updated Article Route

--- backend/routes/api/article.js
+++ backend/routes/api/article.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import auth from '../../middlewares/auth';
+import articleCtrl from '../../controllers/article';
 
 const router = express.Router();
 
 
-const articleCtrl = require('../../controllers/article');
+router.get('/articles', auth, articleCtrl.getAllArticles);
 
 router.post('/articles', auth, articleCtrl.createArticle);
+
 router.put('/articles/:id', auth, articleCtrl.editArticle);
+
 router.delete('/articles/:id', auth, articleCtrl.deleteArticle);
+
 router.post('/articles/:id/comment', auth, articleCtrl.commentArticle);
 
 
