7d0a2c151ba55cb5b5a03d1dbfcd61d318fd7d99
Kelvin Isievwore <kisievwore@seamfix.com>
2019-11-21 01:37:06 +0100

    feat(getGif): Implemented Route To Get A Specific Gif

--- backend/routes/api/gif.js
+++ backend/routes/api/gif.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import auth from '../../middlewares/auth';
+import gifCtrl from '../../controllers/gif';
 
 const router = express.Router();
 
-const gifCtrl = require('../../controllers/gif');
-
 router.post('/gifs', auth, gifCtrl.createGif);
+
+router.get('/gifs/:id', auth, gifCtrl.getGif);
+
 router.delete('/gifs/:id', auth, gifCtrl.deleteGif);
+
 router.post('/gifs/:id/comment', auth, gifCtrl.commentGif);
 
 
